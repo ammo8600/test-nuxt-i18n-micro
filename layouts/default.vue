5da1d6141213df9c75f40d2049eb5fd71415023c
Ahmed Khatri <ammo8600@gmail.com>
2024-11-05 14:57:06 +0500

    inital commit

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+    Layout
+     <slot />
+</template>
+
+<script setup>
+  
+    // console.log(head)
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,25 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  // extends: '../base-ui',
+  // ssr: true,
+  modules: ['nuxt-i18n-micro'],
+  i18n: {
+    debug: true,
+    locales: [
+      {code: 'en', iso: 'en', disabled: false},
+      {code: 'fr', iso: 'fr', disabled: false},
+      {code: 'de', iso: 'de', disabled: false},
+    ],
+    meta: true,
+    defaultLocale: 'en',    
+    translationDir: 'locales',
+    autoDetectLanguage: false,
+    autoDetectPath: '/',
+    metaBaseUrl: 'https://example.com',
+    includeDefaultLocaleRoute: true,
+  },
+})
